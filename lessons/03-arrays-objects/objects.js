df2e5cf512cd3a05230066eb9993b1581c53725c
Sido <sidonie444@gmail.com>
2020-03-18 23:10:00 +0000

    added comment object.js

--- lessons/03-arrays-objects/objects.js
+++ lessons/03-arrays-objects/objects.js
@@ -6,15 +6,30 @@ const jasper = {
   "favourite Foods": ["Sardines", "Tuna"]
 };
 
-console.log(jasper.name);
-console.log(jasper.age);
+console.log(jasper.name); // Jasper
+console.log(jasper.age); //2
+console.log(jasper);
+// {
+// name: 'Jasper',
+//  age: 2,
+//**    breed: 'Exotic shorthair',
+//        'favourite Foods': ['Sardines', 'Tuna']
+// }
+
+jasper.BFF = "Archibald"; // jasper.BFF = "Archibald"を追加
 console.log(jasper);
 
-jasper.BFF = "Archibald";
+// {
+//  name: 'Jasper',
+//    age: 2,
+//      breed: 'Exotic shorthair',
+//        'favourite Foods': ['Sardines', 'Tuna'],
+//          BFF: 'Archibald'
+// }
 
-console.log(jasper);
 
-console.log(jasper["favourite Foods"]);
+console.log(jasper["favourite Foods"]); // ['Sardines', 'Tuna']
+
 
 const curtain = {
   name: "Curtain",
@@ -25,6 +40,14 @@ const curtain = {
 
 const cats = [jasper, curtain];
 
-console.log(cats[0].name);
-console.log(cats[1]["favourite Foods"].push("Berries"));
+ // console.log(cats[0].name); // Jasper
+
+
+console.log(cats[1]["favourite Foods"].push("Berries")); // 3
 console.log(cats[1]);
+// {
+//  name: 'Curtain',
+//    age: 3,
+//      breed: 'Siamese',
+//        'favourite Foods': ['Chocolate', 'Milk', 'Berries']
+// }
