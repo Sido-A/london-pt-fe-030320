971b01f470a0e4a33b790004ea11049b4d653bbb
Sido <sidonie444@gmail.com>
2020-03-19 20:42:29 +0000

    exercise 18

--- 18-creating-arrays-exercise/index.js
+++ 18-creating-arrays-exercise/index.js
@@ -105,6 +105,7 @@ console.log(hasTwenty);
 
 // const numbers = [1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1];
 
+//https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/find
 
 const moreThanFour = numbers.find(number => number > 4);
 console.log(moreThanFour);
