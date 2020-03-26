59c8eef83a280e948faedc9a515773bab488d517
Sido <sidonie444@gmail.com>
2020-03-26 15:01:57 +0000

    exercise 29

--- 29-arrow-functions-exercise/index.js
+++ 29-arrow-functions-exercise/index.js
@@ -9,12 +9,32 @@
  * and return true if they are equal or false if not
  */
 
+ const isEqual = (num1, num2) => {
+     if (num1 === num2) {
+         return true;         
+     } else {
+         return false;
+     }
+ }
+
+ console.log(isEqual(1,2));
+
 /**
  * Exercise 2
  *
  * create an arrow function {isLessOrEqual0} which takes number as a param
  * and return true if it equals or less than 0 or false if not
  */
+
+ const isLessOrEqual0 = numbers => {
+   if (numbers <= 0) {
+     return true;
+   } else {
+       return false;
+   }
+ }
+
+ console.log(isLessOrEqual0(-1));
 
 /**
  * Exercise 3
@@ -27,6 +47,21 @@
  * so you will not break your function if you forget to pass params
  */
 
+ const timeToSeconds = (hours = 0, minutes = 0) => {
+     hours = hours*3600; // 1hr = 3600
+     // console.log(hours);
+     minutes = minutes*60; // 1min = 60
+     // console.log(minutes);
+
+     let seconds = hours + minutes;
+     return seconds;
+ 
+ }
+
+ console.log(timeToSeconds(1,31))
+
+
+
 /**
  * Exercise 4
  *
@@ -36,6 +71,15 @@
  * pay then return true otherwise return false
  */
 
+ const isGameWorthTheCandle = (probability, prize, pay) => {
+     if (probability * prize > pay){
+         return true;
+     } else {
+         return false;
+     }
+
+ }
+
 /**
  * Exercise 5
  *
@@ -43,3 +87,27 @@
  * as parameters and returns the string which is longest. If the strings are
  * equal in length return the string "Equal!"
  */
+
+ const longerString = (stringOne, stringTwo) =>{
+
+    if (stringOne.length > stringTwo.length ) {
+        return stringOne;        
+    } else if (stringOne.length < stringTwo.length) {
+        return stringTwo       
+    } else {
+        return "Equal!";
+    }     
+ }
+
+ console.log(longerString("Worlds","World"));
+
+
+ /**
+ function compareStr(string1, string2) {
+            const result = string1.localeCompare(string2);
+            return result;
+ }
+
+ console.log(compareStr("Worlds","World"));
+
+ **/
