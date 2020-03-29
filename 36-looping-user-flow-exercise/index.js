c7dcffc9750da24be4dafab69b0208398dc04da3
Sido <sidonie444@gmail.com>
2020-03-29 20:54:21 +0100

    exercise 36

--- 36-looping-user-flow-exercise/index.js
+++ 36-looping-user-flow-exercise/index.js
@@ -1,10 +1,23 @@
 // install and  import "readline-sync" npm package before you do exercises
+const readlineSync = require("readline-sync");
+
 
 let selectedItems = {
 	book: "",
 	movie: "",
 	nextTrip: ""
 };
+
+const books = [
+  "My Dark Vanessa",
+  "Uncanny Valley",
+  "Weather",
+  "The night watchman",
+  "All adults here",
+  "Dear Edward",
+  "Grown ups"
+];
+
 
 const movies = [
 	"The Invisible Man",
@@ -16,15 +29,6 @@ const movies = [
 	"Sonic"
 ];
 
-const books = [
-	"My Dark Vanessa",
-	"Uncanny Valley",
-	"Weather",
-	"The night watchman",
-	"All adults here",
-	"Dear Edward",
-	"Grown ups"
-];
 
 const countries = [
 	"Italy",
@@ -46,6 +50,23 @@ const countries = [
  * NOTE: You need to add option to go back, to main menu
  */
 
+const chooseBook = () => {
+	
+	let indexOfBook = readlineSync.keyInSelect(books, "Which book?", {
+    cancel: "Return t top"
+  });
+	console.log("Ok, " + books[indexOfBook] + " has been added to your item.");
+	selectedItems.book = books[indexOfBook];
+
+
+ if (indexOfBook == -1) {
+	 chooseTopMenu();	 
+ 	}
+	  chooseTopMenu();	 
+}
+//  chooseBook();
+//  console.log(selectedItems);
+
 /**
  * Exercise 2
  *
@@ -55,6 +76,23 @@ const countries = [
  *
  * NOTE: You need to add option to "go back", to main menu
  */
+
+ 
+const chooseMovie= () => {
+  let indexOfMovie = readlineSync.keyInSelect(movies, "Which movie?", {cancel: "Return to top"});
+
+  console.log("Ok, " + movies[indexOfMovie] + " has been added to your item.");
+  selectedItems.movie = movies[indexOfMovie];
+
+  if (indexOfMovie == -1) {
+    chooseTopMenu();
+  }
+  chooseTopMenu();
+};
+
+// chooseMovie();
+
+// console.log(selectedItems);
 
 /**
  * Exercise 3
@@ -66,6 +104,23 @@ const countries = [
  * NOTE: You need to add option to go back, to main menu
  */
 
+ 
+const chooseTrip = () => {
+  let indexOfTrip = readlineSync.keyInSelect(countries, "Which movie?", {
+    cancel: "Return to top"
+  });
+  console.log("Ok, " + countries[indexOfTrip] + " has been added to your item.");
+  selectedItems.nextTrip = countries[indexOfTrip];
+
+  if (indexOfTrip == -1) {
+    chooseTopMenu();
+  }
+  chooseTopMenu();
+};
+
+// chooseTrip();
+// console.log(selectedItems);
+
 /**
  * Exercise 4
  *
@@ -73,3 +128,45 @@ const countries = [
  * so user can pick one. User also should have the option "Exit".
  * When the user pick "Exit", log selected items.
  */
+
+ 
+
+const chooseTopMenu = () => {
+	const topMenu = ["Books", "Movies", "Next trip"],
+	pickCategoryIndex = readlineSync.keyInSelect(topMenu, "Pick a category?", {cancel: "Exit"});
+
+	if (pickCategoryIndex +1 == 1) {
+	chooseBook();
+	
+  } else if (pickCategoryIndex +1 == 2) {
+	chooseMovie();
+	
+  } else if (pickCategoryIndex +1 == 3) {
+	chooseTrip();
+	
+  } else if(pickCategoryIndex == -1) {
+	  console.log(selectedItems);
+
+  }
+}
+	chooseTopMenu();
+
+	// const friends = ["Rachel", "Ross", "Chandler", "Monica", "Joey", "Phoebe"];
+
+//   function chooseAFriend() {
+//     console.log("Here are the friends");
+//     friends.forEach((friend, index) => {
+//       console.log(`${index + 1}: ${friend}`);
+//     });
+
+//     const choice = readlineSync.question("Make your choice");
+//     const choiceAsNumber = parseInt(choice);
+//     const chosenFriend = friends[choiceAsNumber - 1];
+
+//     if (chosenFriend !== undefined) {
+//       console.log(`You chose ${chosenFriend}!`);
+//     } else {
+//       console.log("That is not a valid choice");
+//       chooseAFriend();
+//     }
+//   }
