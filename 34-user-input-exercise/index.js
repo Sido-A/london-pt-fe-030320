9bdd88258f9601cd8a64375392a772f41282f5a9
Sido <sidonie444@gmail.com>
2020-03-29 18:55:47 +0100

    complete

--- 34-user-input-exercise/index.js
+++ 34-user-input-exercise/index.js
@@ -1,10 +1,15 @@
-// install and  import "readline-sync" npm package before you do exercises
+  // start simple with a loop that prompts the question after each input
+  // if you're stuck always try to break down the problem is smaller pieces
 
-/**
- * Exercise 1
- *
- * ask user for a name and assign a response to variable {name}
- */
+
+// install and  import "readline-sync" npm package before you do exercises
+const readlineSync = require("readline-sync");
+// /**
+//  * Exercise 1
+//  *
+//  * ask user for a name and assign a response to variable {name}
+//  */
+const name = readlineSync.question("May I have your name? ");
 
 //===== DO NOT TOUCH THIS BLOCK =====
 console.log(`Hi ${name}!`);
@@ -15,7 +20,6 @@ console.log("=====================");
 
 /**
  * Exercise 2
- *
  * offer user to do some math, ask to provide a math symbol and
  * offer a few options: +, -, * or /
  * Save response to {selectedSymbol} variable.
@@ -23,6 +27,19 @@ console.log("=====================");
  * NOTE: if the user will respond with wrong value, ask again, until
  * you get correct symbol
  */
+
+const symbols = ['+' , '-', '*', '/']
+
+let selectedSymbol = readlineSync.question(`Do you want to sum, subtract, multiply or divide? Please input one symbol from ${symbols.join(", ")} `); 
+
+const acceptedSymbol = () => {
+  selectedSymbol = readlineSync.question(
+    `Invalid input. Please choose again ${symbols.join(", ")} `);
+};
+
+while(!symbols.includes(selectedSymbol)) { // if selectedSymbol does not(!) include symbols 
+  acceptedSymbol();
+}
 
 /**
  * Exercise 3
@@ -33,6 +50,11 @@ console.log("=====================");
  * you get a number
  */
 
+let number1 = readlineSync.questionFloat("Input your first number: ", {limitMessage: "Input invalid please type any number again"}); 
+// console.log(number1);
+// Integer is  1 == 1.0 technically a float 
+
+
 /**
  * Exercise 4
  *
@@ -42,6 +64,11 @@ console.log("=====================");
  * you get a number
  */
 
+ let number2 = readlineSync.questionFloat("Input your second number: ", {
+   limitMessage: "Input invalid please type any number again"
+ });
+
+
 /**
  * Exercise 5
  *
@@ -50,6 +77,29 @@ console.log("=====================");
  *
  * show the result to the user
  */
+let result = 0;
+
+const userInputTotal = () => {
+
+     if (selectedSymbol == '+') {
+          result = number1 + number2;
+          
+     } 
+     else if(selectedSymbol == '-'){
+          result = number1 - number2;
+
+     }
+     else if (selectedSymbol == "*") {
+          result = number1 * number2;
+     }
+     else {
+          result = number1 / number2;
+     }
+}
+
+userInputTotal();
+
+
 
 console.log("=====================");
 console.log(`Here you go, the result is ${result}`);
