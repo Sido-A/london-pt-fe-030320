644734c9fd6911bc58aa0d3939a56b51e2e4343b
Sido <sidonie444@gmail.com>
2020-03-29 19:38:26 +0100

    exercise 35

--- 35-user-profile-exercise/index.js
+++ 35-user-profile-exercise/index.js
@@ -1,11 +1,18 @@
 // install and  import "readline-sync" npm package before you do exercises
+const readlineSync = require('readline-sync');
+const chalk = require("chalk");
+
+
 
 const user = {};
+
 /**
  * Exercise 1
  *
  * ask user for a first name and save response to {user.firstName}
  */
+
+ user.firstName = readlineSync.question('What is your first name? : ');
 
 /**
  * Exercise 2
@@ -13,6 +20,10 @@ const user = {};
  * ask user for a last name and save response to {user.lastName}
  */
 
+  user.lastName = readlineSync.question("What is your last name? : ");
+
+
+  // console.log(user);
 console.log(`Hello ${user.firstName} ${user.lastName}!`);
 
 /*
@@ -20,6 +31,8 @@ console.log(`Hello ${user.firstName} ${user.lastName}!`);
  *
  * ask user for a age and save response to {user.age}
  */
+ user.age = readlineSync.questionFloat("What is your age : ");
+
 
 /*
  * Exercise 4
@@ -27,14 +40,16 @@ console.log(`Hello ${user.firstName} ${user.lastName}!`);
  * ask user for a email and save response to {user.email}
  */
 
+ user.email = readlineSync.question("What is your email address : ");
+
+
 /**
  * OPTIONAL:
  *
  * use "chalk" to print out user data with colours :)
  */
 
-console.log(
-  `Here what we have, your full name is ${user.firstName} ${user.lastName}. 
-    You are ${user.age} and if we need to contact with you we can send 
-    a letter to ${user.email} `
+console.log(chalk`Here what we have, your full name is {blue.bold ${user.firstName}} {blue.bold ${user.lastName}}. 
+    You are {red.bold ${user.age}} and if we need to contact with you we can send 
+    a letter to {inverse.bold ${user.email}}.`
 );
