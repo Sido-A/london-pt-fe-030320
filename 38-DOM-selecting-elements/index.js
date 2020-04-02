5f70f720d59da0366146a34f87da3e46da76cb62
Sido-A <sidonie.abe.uk@gmail.com>
2020-04-02 14:00:22 +0100

    exe 38 done

--- 38-DOM-selecting-elements/index.js
+++ 38-DOM-selecting-elements/index.js
@@ -5,11 +5,15 @@
  * to variable {paragraphs}
  */
 
+ const paragraphs = document.getElementsByTagName("p");
+
 /**
  * Exercise 2
  * select the first p tag with getElementsByTagName and assign it
  * to variable {firstParagraph}
  */
+
+const firstParagraph = document.getElementsByTagName("P")[0];
 
 /**
  * Exercise 3
@@ -20,12 +24,22 @@
  * ATTENTION: check the type of {paragraphs} before doing forEach
  */
 
+// https://www.gavsblog.com/blog/htmlcollection-foreach-loop-convert-object-to-array-javascript
+
+const logAllParagraphs = () => {
+    const convertParagraphs = Array.from(document.getElementsByTagName("P"));
+    convertParagraphs.forEach(paragraph => {
+        console.log(paragraph);
+    })    
+}
 /**
  * Exercise 4
  *
  * select all elements with class "text" with getElementsByClassName
  * and assign it to variable {textElements}
  */
+
+const textElements = document.getElementsByClassName("text");
 
 /**
  * Exercise 5
@@ -34,12 +48,18 @@
  * with getElementsByClassName and assign it to variable {greenText}
  */
 
+const greenText = document.getElementsByClassName(".text.green");
+
 /**
  * Exercise 6
  *
  * select element where id is 'textId-3'(use getElementById) and assign it
  * to variable {thirdText}
  */
+
+const thirdText = document.getElementById("kitten");
+
+
 
 /* ==================================================== */
 /*                  querySelector section               */
@@ -51,18 +71,22 @@
  *
  * select the fourth "li" element and save it to {fourthListElement} variable
  */
-
+const fourthListElement = document.querySelector("li:nth-child(4)");
 /**
  * Exercise 8
  *
  * select all "li" elements and save it to {listElements} variable
  */
 
+const listElements = document.querySelectorAll("li");
 /**
  * Exercise 8
  *
  * select all "a" elements that are inside "ul" and assign it {listLinks}
  */
+
+ const listLinks = document.querySelectorAll("ul > a");
+
 
 /**
  * Exercise 9
@@ -70,6 +94,8 @@
  * select elements where the "data-community-name" attribute
  * is "codersinhoods" and assign it {communityElements}
  */
+// https://hyperts.net/javascript-data-select/
+const communityElements = document.querySelectorAll(`[data-community-name*="codersinhoods"]`);
 
 /**
  * Exercise 10
@@ -78,6 +104,8 @@
  * {elementsForTesting}
  */
 
+const elementsForTesting = document.querySelectorAll(".test");
+
 /**
  * Exercise 11
  *
@@ -85,9 +113,14 @@
  * {fifthTest}
  */
 
+const fifthTest = document.querySelector("#test-5");
+
+
 /**
  * Exercise 12
  *
  * select all the elements which are siblings of an element
  * with the class "links" and assign it to a variable {nextElements}
  */
+
+const nextElements = document.querySelectorAll(".links ~ li");
