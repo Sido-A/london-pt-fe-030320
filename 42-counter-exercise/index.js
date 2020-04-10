f83b38dee71fd8cd22c76bc0a2e41b32571f0a4d
Sido-A <sidonie.abe.uk@gmail.com>
2020-04-10 17:04:34 +0100

    exe 42

--- 42-counter-exercise/index.js
+++ 42-counter-exercise/index.js
@@ -1,5 +1,8 @@
 let step = 0;
 let counter = 0;
+const counterValue = document.querySelector(".counter_value");
+
+
 
 /**
  * Exercise 1
@@ -11,7 +14,21 @@ let counter = 0;
  * NOTE: ".counter_value" should always represent the current value of {counter}
  */
 
-/**
+ const action = (symbol) => {
+     if (symbol == "+") {
+         counter += step;  
+         counterValue.value = counter;       
+         counterValue.innerText = counterValue.value;
+        
+     } else if(symbol == "-"){
+         counter -= step;
+         counterValue.value = counter;       
+         counterValue.innerText = counterValue.value;
+     }
+ }
+ action();
+
+ /**
  * Exercise 2
  *
  * When the user clicks on "Set step" button, you need to:
@@ -23,6 +40,26 @@ let counter = 0;
  *
  */
 
+const setStep = () => {
+    const stepForm = document.querySelector(".step_form");
+        stepForm.addEventListener("submit", ()=> {
+        const currentStepIs = document.querySelector(".step_value"); // current step is: 
+        const stepInput = document.querySelector("#step"); //Step [input]
+        // update Step[input] value on let step = 0;
+        step = Number(stepInput.value); //Step [input]
+
+         // giving the Step[input number] to Current Step is: #        currentStepIs.innerText = step;  
+        currentStepIs.value = step;  
+        currentStepIs.innerText = currentStepIs.value;
+            // console.log(typeof currentStepIs.value);
+            // console.log(typeof currentStepIs.innerText);      
+
+        // reset the Step[input] value to 1
+        stepInput.value = 1;        
+    })
+}
+setStep();
+
 /**
  * Exercise 3
  *
@@ -31,6 +68,12 @@ let counter = 0;
  *
  * NOTE: remember to use your {action} function
  */
+const decrement = document.querySelector("#decrement")
+     decrement.addEventListener("click", () =>{
+         action("-");
+        //  console.log(counter);         
+     })     
+ 
 
 /**
  * Exercise 4
@@ -40,6 +83,11 @@ let counter = 0;
  *
  * NOTE: remember to use your {action} function
  */
+const increment = document.querySelector("#increment")
+    increment.addEventListener("click", () => {
+        action("+");
+        // console.log(counter);
+    })
 
 /**
  * Exercise 5
@@ -49,6 +97,14 @@ let counter = 0;
  *
  * NOTE: ".counter_value" should represent current state of counter
  */
+    const autoDecrementButton = document.querySelector("#auto_decrement")
+    autoDecrementButton.addEventListener("click", () => {
+        setInterval(() => {
+            action("-");
+            // console.log(counter);            
+            // console.log(counterValue);   
+        }, 1000);
+    })
 
 /**
  * Exercise 6
@@ -59,9 +115,25 @@ let counter = 0;
  * NOTE: ".counter_value" should represent current state of counter
  */
 
+    const autoIncrementButton = document.querySelector("#auto_increment")
+    autoIncrementButton.addEventListener("click", () => {
+        setInterval(() => {
+            action("+");
+            // console.log(counter);            
+            // console.log(counterValue);               
+        }, 1000);
+    })
+
+
 /**
  * Exercise 7
  *
  * when the user clicks on "#stop_auto",
  * the auto counter should stop
  */
+
+     const stopAutoButton = document.querySelector("#stop_auto");
+     stopAutoButton.addEventListener("click", () =>{
+         console.log(autoDecrementButton);         
+         console.log("hi");    
+     })     
