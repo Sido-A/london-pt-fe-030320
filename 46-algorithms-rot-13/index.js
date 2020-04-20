56a0f7a4fb4d571b6676a5b6916260a7b8527b66
Sido-A <sidonie.abe.uk@gmail.com>
2020-04-20 01:50:32 +0100

    exe 46 done

---
+++ 46-algorithms-rot-13/index.js
@@ -0,0 +1,56 @@
+const textarea = document.querySelector("textarea");
+const encrypt = document.getElementById("encrypt");
+const decrypt = document.getElementById("decrypt");
+const result = document.querySelector(".result");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click",event =>{    
+    if (event.target === encrypt) {
+        result.innerText = rot13(textarea.value);  
+
+    } else if (event.target === decrypt) {
+        result.innerText = rot13(textarea.value);  
+    }
+});
+
+const rot13 = (text) => {
+    let userInput = [];
+    // split the passed text
+    userInput = text.split("");
+
+    // 
+    return userInput.map.call(userInput, function (splitText) {
+        unicode = splitText.charCodeAt(0);
+
+        // A=> 65, N=> 78 || a=> 97, n=> 110 if the text unicode match plus 13
+        // B=> 66 + 13 will convert to O(79)
+        if ((65 <= unicode && unicode < 78) || (97 <= unicode && unicode < 110)) {
+            
+            return String.fromCharCode(unicode + 13);
+        // N=> 78, Z=> 90 || n=> 110, z=> 122 if the text unicode match minus 13
+        // q=> 113 -13 will convert to d(100)
+        } else if ((78 <= unicode && unicode <= 90) || (110 <= unicode && unicode <= 122)) {
+            return String.fromCharCode(unicode - 13);
+        }
+
+        // return space and other unicode that's not match above is statement
+        return String.fromCharCode(unicode);
+        // concat the text that was split at the beginning
+    }).join('');
+}
+
+// function anotherRot13(str) {
+//     var i = [];
+//     i = str.split('');
+//     return i.map.call(i, function (char) {
+//         x = char.charCodeAt(0);
+//         if ((65 <= x && x < 78) || (97 <= x && x < 110)) {
+//             return String.fromCharCode(x + 13);
+//         }
+//         else if ((78 <= x && x <= 90) || (110 <= x && x <= 122)) {
+//             return String.fromCharCode(x - 13);
+//         }
+//         return String.fromCharCode(x);
+//     }).join('');
+// }
+// console.log(anotherRot13("Uryyb Jbeyq!"));
