c9622f14054a1c25a9627956759b48ac936a20d4
Sido-A <sidonie.abe.uk@gmail.com>
2020-04-20 01:52:22 +0100

    exe 46 done

--- 46-algorithms-rot-13/index.js
+++ 46-algorithms-rot-13/index.js
@@ -18,39 +18,23 @@ const rot13 = (text) => {
     // split the passed text
     userInput = text.split("");
 
-    // 
+    // check each letter and return
     return userInput.map.call(userInput, function (splitText) {
         unicode = splitText.charCodeAt(0);
 
         // A=> 65, N=> 78 || a=> 97, n=> 110 if the text unicode match plus 13
         // B=> 66 + 13 will convert to O(79)
-        if ((65 <= unicode && unicode < 78) || (97 <= unicode && unicode < 110)) {
-            
+        if ((65 <= unicode && unicode < 78) || (97 <= unicode && unicode < 110)) {            
             return String.fromCharCode(unicode + 13);
+
         // N=> 78, Z=> 90 || n=> 110, z=> 122 if the text unicode match minus 13
         // q=> 113 -13 will convert to d(100)
         } else if ((78 <= unicode && unicode <= 90) || (110 <= unicode && unicode <= 122)) {
             return String.fromCharCode(unicode - 13);
         }
-
         // return space and other unicode that's not match above is statement
         return String.fromCharCode(unicode);
+
         // concat the text that was split at the beginning
     }).join('');
 }
-
-// function anotherRot13(str) {
-//     var i = [];
-//     i = str.split('');
-//     return i.map.call(i, function (char) {
-//         x = char.charCodeAt(0);
-//         if ((65 <= x && x < 78) || (97 <= x && x < 110)) {
-//             return String.fromCharCode(x + 13);
-//         }
-//         else if ((78 <= x && x <= 90) || (110 <= x && x <= 122)) {
-//             return String.fromCharCode(x - 13);
-//         }
-//         return String.fromCharCode(x);
-//     }).join('');
-// }
-// console.log(anotherRot13("Uryyb Jbeyq!"));
