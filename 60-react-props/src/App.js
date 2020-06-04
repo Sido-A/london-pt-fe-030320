06ac072ad1aa226e053b846db912abc0547ae17f
Sido-A <sidonie.abe.uk@gmail.com>
2020-06-04 23:38:05 +0100

    done exe 60

--- 60-react-props/src/App.js
+++ 60-react-props/src/App.js
@@ -6,14 +6,35 @@ import UserCard from "./components/UserCard/UserCard";
  * Exercise
  * 1. fetch data and store it in {users} state.
  * 2. Pass user data as {user} prop for UserCard component and render all users.
- * 3. In UserCard component, render user name, and user email. 
+ * 3. In UserCard component, render user name, and user email.
  * Inside UserCard add class "card" to wrapper.
- * 
+ *
  * NOTE: DB url - http://localhost:3000/contacts
  */
+const URL = "http://localhost:3000/contacts";
+const getUser = async () => {
+  return await fetch(URL)
+    .then((res) => res.json())
+    .then((res) => res);
+};
 
 const App = () => {
+  const [users, setUsers] = useState([]);
 
+  if (users.length === 0) {
+    // getUser().then(res=> setUsers(res))    
+    fetch(URL)
+      .then((res) => res.json())
+      .then((res) => setUsers(res));
+  }
+
+  return (
+    <div>
+      {users.map((user) => (
+        <UserCard user={user}/>
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- 60-react-props/src/components/UserCard/UserCard.js
+++ 60-react-props/src/components/UserCard/UserCard.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const UserCard = () => {
-  return <div></div>;
+const UserCard = (props) => {
+  const {user} = props;
+  const { name, email, id } = user;
+
+  return (
+    <div className="card" key={id}>
+          <p className="userName">
+            <strong>Name:</strong> {name}</p>
+          <p className="userEmail">
+            <strong>Email:</strong> {email}</p>
+    </div>
+  );
 };
 
 export default UserCard;
